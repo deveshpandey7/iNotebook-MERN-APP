945dd6eb2b2bada6cb3f146a568a1026880fded1
Devesh Pandey <deveshpandey607@gmail.com>
2024-02-13 18:21:41 +0530

    working on notes end points

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectToMongo = require('./db');
 const authRoutes = require('./routes/auth');
+const notesRoutes = require('./routes/notes');
 
 const app = express();
 const port = 5000;
@@ -13,6 +14,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api/auth', authRoutes); // Use authRoutes directly as a route
+app.use('/api/notes', notesRoutes); // Use notesRoutes directly as a route
 
 // Start the server
 app.listen(port, () => {
