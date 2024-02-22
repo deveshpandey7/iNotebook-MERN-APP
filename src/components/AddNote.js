ebf943180b2cc3e73326fa971ac24da028b2750b
Devesh Pandey <deveshpandey607@gmail.com>
2024-02-22 19:08:19 +0530

    Login feature added

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -11,6 +11,7 @@ const AddNote = () => {
     const handleClick = (e)=>{
         e.preventDefault();
        addNote(note.title, note.description, note.tag);
+       setNote({title: "", description: "", tag: ""}); 
     }
 
     const onChange = (e) =>{
@@ -24,15 +25,15 @@ const AddNote = () => {
      <form className="my-3">
         <div className="mb-3">
           <label htmlFor="title" className="form-label">Title</label>
-          <input type="text" className="form-control" id="title" name="title" aria-describedby="emailHelp" onChange={onChange} minLength={3} required/>
+          <input type="text" className="form-control" id="title" name="title" value={note.title} aria-describedby="emailHelp" onChange={onChange} minLength={3} required/>
         </div>
         <div className="mb-3">
           <label htmlFor="description" className="form-label">Description</label>
-          <input type="text" className="form-control" id="description" name="description" onChange={onChange} minLength={5} required/>
+          <input type="text" className="form-control" id="description" name="description" value={note.description} onChange={onChange} minLength={5} required/>
         </div>
         <div className="mb-3">
           <label htmlFor="tag" className="form-label">Tag</label>
-          <input type="text" className="form-control" id="tag" name="tag" onChange={onChange} />
+          <input type="text" className="form-control" id="tag" name="tag" value={note.tag} onChange={onChange} />
         </div>
         <button disabled={note.title.length<3 || note.description.length<5} type="submit" className="btn btn-primary" onClick={handleClick}>Add Note</button>
       </form>
